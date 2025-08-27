2ce0120164ca4fc912c890ae6a69c3817d07dc7c
Osaru Ize-Iyamu <osaru.izeiyamu@shopify.com>
2025-08-27 13:05:32 -0400

    adds read custom data app and extension code

---
+++ extensions/read-custom-data/src/Checkout.jsx
@@ -0,0 +1,33 @@
+import '@shopify/ui-extensions/preact';
+import {render} from "preact";
+import {useAppMetafields} from "@shopify/ui-extensions/checkout/preact"
+
+// 1. Export the extension
+export default function() {
+  render(<Extension />, document.body)
+}
+
+function Extension() {
+  // Get the product ID from the cart line item
+  const {merchandise: {product: {id: productId}}} = shopify.lines.value[0];
+  
+  // Use the merchant-defined metafield for watering instructions and map it to a cart line
+  const [wateringMetafield] = useAppMetafields( {
+    type: "product",
+    namespace: "instructions",
+    key: "watering",
+  }).filter(
+    (entry) =>  productId.endsWith(entry.target.id)
+  );
+  
+  // Render the watering instructions if applicable
+  if (wateringMetafield?.metafield?.value) {
+    return (
+        <s-text>
+          {wateringMetafield?.metafield?.value}
+        </s-text>
+      );
+  }
+  
+  return null;
+}
